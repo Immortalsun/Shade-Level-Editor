f3bd9ba9993534f623261b00058bb01be9571312
Immortalsun <maashes.p@gmail.com>
2016-04-30 11:05:45 -0400

    Initial commit

---
+++ ShadeGameLevelEditor/Model/Level.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadeGameLevelEditor.Models
+{
+    public class Level
+    {
+        //Fields
+
+        //Constructor
+
+        //Methods
+
+        //Events
+
+    }
+}
