5ada993e5e0463f4446652a34a2f2f34264147a7
Immortalsun <maashes.p@gmail.com>
2016-04-30 11:28:44 -0400

    Begin adding architecture

--- ShadeGameLevelEditor/Model/Level.cs
+++ ShadeGameLevelEditor/Model/Level.cs
@@ -3,18 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace ShadeGameLevelEditor.Models
 {
-    public class Level
+    public class Level : IXmlSerializable
     {
         //Fields
+        private string _bgFilePath;
+        private List<Platform> _platformBlocks;
 
         //Constructor
+        public Level()
+        {
+            _bgFilePath = String.Empty;
+            _platformBlocks = new List<Platform>();
+        }
+
+        public Level(string levelFilePath)
+        {
+
+        }
+
+        public Level(Level level)
+        {
+            _bgFilePath = level._bgFilePath;
+            _platformBlocks = level._platformBlocks;
+        }
 
         //Methods
 
+        private void LoadLevelFile(string levelFile)
+        {
+
+        }
+
+        private void SaveLevel(string saveLoc)
+        {
+
+        }
+
+        public void AddPlatformByCoordinates(int left, int top, int width, int height)
+        {
+            _platformBlocks.Add(new Platform(left,top,width,height));
+        }
+
+        public void AddPlatformByBlock(Platform block)
+        {
+            _platformBlocks.Add(block);
+        }
+
+
         //Events
 
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ShadeGameLevelEditor/Model/Platform.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ShadeGameLevelEditor.Model
+{
+    public class Platform : IXmlSerializable
+    {
+        //Fields
+        private int _x, _y, _width, _height;
+        //Constructor
+        public Platform()
+        {
+            _x = 0;
+            _y = 0;
+            _width = 0;
+            _height = 0;
+        }
+
+        public Platform(int x, int y, int width, int height)
+        {
+            _x = x;
+            _y = y;
+            _width = width;
+            _height = height;
+        }
+
+        public Platform(Platform p)
+        {
+            _x = p._x;
+            _y = p._y;
+            _width = p._width;
+            _height = p._height;
+        }
+
+        //Methods
+        //Events
+        public XmlSchema GetSchema()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
